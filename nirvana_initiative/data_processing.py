8ed2eaa0b03d1b47e385e9f6e2346048c22bc190
TachiTel <jordan@multilingualbus.com>
2024-02-10 16:38:29 +0000

    update

--- nirvana_initiative/data_processing.py
+++ nirvana_initiative/data_processing.py
@@ -1,5 +1,6 @@
 import os
 import pandas
+import api_networking
 
 class DP:
     """Data Processing"""
@@ -18,20 +19,66 @@ class DP:
         else:
             self.folder = cwd
         self.file = self.folder + "/" + self.filename
+        self.runner()
 
     def csv_reader(self):
         self.data = pandas.read_csv(self.file)
         print(self.data)
         return self.data
 
+    def txt_reader(self, file):
+        dat = []
+        with open(file, 'r') as f:
+            while line := f.readline():
+                dat.append(line.rstrip())
+                #print(line.rstrip(), '/line')
+        f.close()
+        return dat
+
     def questions(self):
         data = self.csv_reader()
         # Questions are kept in [2]
-        return data[2]
+        return [data[col].tolist() for col in data.columns][1]
+    
+    def runner(self):
+        #dat = self.questions()
+        dat = self.txt_reader("../data/CFR-2023-title14-vol1.txt")
+        #print(dat)
+        messages = []
+        n = 0
+        for d in dat:
+            if d=="":
+                continue
+            n += 1
+            if n%1000==0:
+                print(n)
+            #print(d)
+            try:
+                messages.append(api_networking.llm_qa(llm_server_url='http://localhost:1234/v1',
+                              message=d, max_tokens=1))
+            except:
+                #print("Failed, your mum")
+                pass
+        
+        print(messages)
         
 
 DP = DP()
 
-DP.start("Aviation Quiz.csv")
 
-DP.csv_reader()
+def csv_reader(file):
+    data = pandas.read_csv(file)
+    print(data)
+    return data
+def find():
+
+    Question = ""
+    Question = input("Stuff to find: ")
+
+    data = []
+    for i in ["../data/CFR-2023-title14-vol1.txt","../data/CFR-2023-title14-vol2.txt","../data/CFR-2023-title14-vol3.txt","../data/CFR-2023-title14-vol4.txt","../data/CFR-2023-title14-vol5.txt"]:
+        data.append(csv_reader(i))
+
+    print(data)
+#DP.start("Aviation Quiz.csv")
+find()
